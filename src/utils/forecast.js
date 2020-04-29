eabf5ce8af54b281c51806c1041fb0bc17677c50
Anuj Singh <anujs787@gmail.com>
2020-04-30 01:28:50 +0530

    Changed api from darksky to weatherstack

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,12 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const url = 'https://api.darksky.net/forecast/f09f93bcb270abb93b62b46bcd7fc238/' + lat + ',' + long
+    const url = 'http://api.weatherstack.com/current?access_key=4a23caa464348c1477d8a0c75e2b8737&query=' + lat + ',' + long
 
     request({ url, json: true }, (error, {body}) => {
-        if(error) {
+        if (error) {
             callback('Unable to connect to network', undefined)
-        } else if(body.error) {
+        } else if (body.error) {
             callback('Unable to find location. try again', undefined)
         } else {
             const temp = body.currently
